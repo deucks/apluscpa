191ab5a0179e5e441465883ffef19655d60188ad
Raajit <cosmickinect1@gmail.com>
2015-12-02 00:43:12 +1100

    changes to index, structure

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta name=viewport content="width=device-width, initial-scale=0.7">
+    </head>
+
+    <body>
+
+    </body>
+
+    <footer>
+
+    </footer>
+
+</html>
