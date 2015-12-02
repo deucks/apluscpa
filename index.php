a9d611f569de14813bea41c258b5c67e94c7a30f
Raajit <cosmickinect1@gmail.com>
2015-12-02 15:29:17 +1100

    fonrt feature section up

--- index.php
+++ index.php
@@ -2,9 +2,47 @@
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <meta name=viewport content="width=device-width, initial-scale=0.7">
+
+
+        <link href='https://fonts.googleapis.com/css?family=Raleway:500,300,600' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/main.css">
     </head>
 
     <body>
+        <div class="feature-section">
+            <div class="feature-background">
+                <img src="https://images.unsplash.com/photo-1424894408462-1c89797f2305?crop=entropy&fit=crop&fm=jpg&h=1000&ixjsv=2.1.0&ixlib=rb-0.3.5&q=80&w=1925">
+            </div>
+            <div class="container">
+                <div class="feature-title-section">
+
+                    <div id="feature-logo">
+                        <img alt="Company Logo" src="images/apluscropedtrans.png">
+                    </div>
+                    <div id="feature-title-links">
+                        <ul class="nav navbar-nav">
+                            <li>
+                                <a href="/index.html">SERVICES</a>
+                            </li>
+                            <li>
+                                <a href="minimal/index.html">ABOUT US</a>
+                            </li>
+                            <li>
+                                <a href="bold/index.html">OUR TEAM</a>
+                            </li>
+                            <li>
+                                <a href="docs/index.html">CONTACT US</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="feature-content">
+                    <p>Helping business owners achieve their goals.</p>
+                    <p>BOOKEEPING × BUSINESS ACCOUNTING × </p>
+                </div>
+            </div>
+        </div>
 
     </body>
 
